6dface63c1c5fdbfccd0554ca5a501f986432f2f
ReddyyZ <41487053+ReddyyZ@users.noreply.github.com>
2020-02-25 23:44:37 -0300

    Add files via upload

---
+++ modules/os_detection.py
@@ -0,0 +1,13 @@
+from scapy.all import *
+
+def scan(target):
+    try:
+        os_ttl = {'Linux/Unix':255,'Linux/Unix 2.0.x kernel':64,'Windows 98':32,'Windows':128}
+        pkg = IP(dst=target,ttl=128)/ICMP()
+        ans, uns = sr(pkg,retry=2,timeout=10,inter=1,verbose=0)
+        target_ttl = ans[0][1].ttl
+        for ttl in os_ttl:
+            if target_ttl == os_ttl[ttl]:
+                return ttl
+    except:
+        return False
---
+++ modules/service_detection.py
@@ -0,0 +1,18 @@
+from nmap_vscan import vscan
+import sys,platform
+
+def scan_service(target,port):
+    if platform.system() == 'Linux':
+        nmap = vscan.ServiceScan('/usr/share/astsu/service_probes')
+    elif platform.system() == 'Windows':
+        nmap = vscan.ServiceScan('C:\\astsu\\service_probes')
+    result = nmap.scan(str(target), int(port), 'tcp')
+    service_name = str(result['match']['versioninfo']['cpename'])
+    
+    service_name = service_name.replace('[','')
+    service_name = service_name.replace(']','')
+    service_name = service_name.replace("'","",2)
+
+    if not service_name:
+        service_name = 'Not found any service'
+    return service_name
