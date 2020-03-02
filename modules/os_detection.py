e722f433b2bcfe334100bae3263c905f04540d36
ReddyyZ <41487053+ReddyyZ@users.noreply.github.com>
2020-03-01 21:52:25 -0300

    Add files via upload

--- modules/os_detection.py
+++ modules/os_detection.py
@@ -2,7 +2,7 @@ from scapy.all import *
 
 def scan(target):
     try:
-        os_ttl = {'Linux/Unix':255,'Linux/Unix 2.0.x kernel':64,'Windows 98':32,'Windows':128}
+        os_ttl = {'Linux/Unix 2.2-2.4 >':255,'Linux/Unix 2.0.x kernel':64,'Windows 98':32,'Windows':128}
         pkg = IP(dst=target,ttl=128)/ICMP()
         ans, uns = sr(pkg,retry=2,timeout=10,inter=1,verbose=0)
         target_ttl = ans[0][1].ttl
