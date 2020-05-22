5eab60b5ee6ff19fd6406716cdc7a09f38089cc8
ReddyyZ <41487053+ReddyyZ@users.noreply.github.com>
2020-05-22 20:24:31 -0300

    1.0 Version

--- modules/os_detection.py
+++ modules/os_detection.py
@@ -1,11 +1,21 @@
 from scapy.all import *
 
-def scan(target):
+def scan(target,interface=None):
     try:
         os_ttl = {'Linux/Unix 2.2-2.4 >':255,'Linux/Unix 2.0.x kernel':64,'Windows 98':32,'Windows':128}
         pkg = IP(dst=target,ttl=128)/ICMP()
-        ans, uns = sr(pkg,retry=2,timeout=10,inter=1,verbose=0)
-        target_ttl = ans[0][1].ttl
+
+        if interface:
+            ans, uns = sr(pkg,retry=5,timeout=3,inter=1,verbose=0,iface=interface)
+        else:
+            ans, uns = sr(pkg,retry=5,timeout=3,inter=1,verbose=0)
+
+        try:
+            target_ttl = ans[0][1].ttl
+        except:
+            print("[-] Host did not respond")
+            return False
+
         for ttl in os_ttl:
             if target_ttl == os_ttl[ttl]:
                 return ttl
--- modules/service_detection.py
+++ modules/service_detection.py
@@ -2,11 +2,16 @@ from nmap_vscan import vscan
 import sys,platform
 
 def scan_service(target,port):
+    return True
+
     if platform.system() == 'Linux':
         nmap = vscan.ServiceScan('/usr/share/astsu/service_probes')
     elif platform.system() == 'Windows':
-        nmap = vscan.ServiceScan('C:\\astsu\\service_probes')
-    result = nmap.scan(str(target), int(port), 'tcp')
+        nmap = vscan.ServiceScan('C:\\Projetos\\Tools\\Network Tool\\service_probes')
+    try:
+        result = nmap.scan(str(target), int(port), 'tcp')
+    except Exception as e:
+        return e
     service_name = str(result['match']['versioninfo']['cpename'])
     
     service_name = service_name.replace('[','')
