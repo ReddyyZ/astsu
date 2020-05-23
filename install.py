16cdae8ffdf07fe2e71961031bd20325b1ef0aa9
ReddyyZ <41487053+ReddyyZ@users.noreply.github.com>
2020-05-23 11:05:19 -0300

    Update install.py

--- install.py
+++ install.py
@@ -4,7 +4,7 @@ machine_os = platform.system()
 
 if machine_os == 'Linux':
     os.system('mkdir /usr/share/astsu')
-    os.system('cp * /usr/share/astsu')
+    os.system('cp -r * /usr/share/astsu')
     os.system('mv /usr/share/astsu/astsu.py /usr/share/astsu/astsu')
     os.system('ln -s /usr/share/astsu/astsu /usr/bin/astsu')
     print('[+] ASTSU installed')
